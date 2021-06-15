0777c19709a1cb9c47612173ced7962a23e29d2a
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-06-14 21:57:35 -0400

    homework

---
+++ viewattractions.js
@@ -0,0 +1,63 @@
+{
+    "_id": "",
+    "name": "Cincinnati Zoo and Botanical Gardens",
+    "description": "Beautiful zoo and gardens with extensive grounds and fauna.",
+    "website": "http://cincinnatizoo.org/",
+    "location": {
+        "_id": "",
+        "address": "3400 Vine Street",
+        "city": "Cincinnati",
+        "state": "Ohio",
+        "zipcode": "45220"
+    },
+    "indoors": true,
+    "__v": 0
+}
+
+{
+    "_id": "",
+    "name": "Newport on the Levee",
+    "description": "Riverside commercial complex with beautiful views, shopping, dining, and entertainment.",
+    "website": "https://www.newportonthelevee.com/",
+    "location": {
+        "_id": "",
+        "address": "1 Levee Way",
+        "city": "Newport",
+        "state": "Kentucky",
+        "zipcode": "41071"
+    },
+    "indoors": true,
+    "__v": 0
+}
+
+{
+    "_id": "",
+    "name": "Ault Park",
+    "description": "Cincinnati's fourth largest park boasting stunning views, gardens, hiking trails, and wedding venues.",
+    "website": "https://www.cincinnatiparks.com/parks-venues/east/ault-park/",
+    "location": {
+        "_id": "",
+        "address": "5090 Observatory Avenue",
+        "city": "Cincinnati",
+        "state": "Ohio",
+        "zipcode": "45208"
+    },
+    "indoors": false,
+    "__v": 0
+}
+
+{
+    "_id": "",
+    "name": "Alms Park",
+    "description": "A hidden gem on top of Mt. Tusculum, Alms Park offers views of the Ohio River, hills, skyline, and scenery of Cincinnati and NKY. A wonderful spot to view Lunken Airport and observe planes coming and going",
+    "website": "https://www.cincinnatiparks.com/parks-venues/east/ault-park/",
+    "location": {
+        "_id": "",
+        "address": "710 Tusculum Avenue",
+        "city": "Cincinnati",
+        "state": "Ohio",
+        "zipcode": "45226"
+    },
+    "indoors": false,
+    "__v": 0
+}
